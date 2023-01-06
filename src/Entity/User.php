5cbacd07859db09f3b6f0f3b7e0e9e7e7b81b019
Lahaxe Hugo <lahaxeh@gmail.com>
2023-01-06 03:02:48 +0100

    profile img nullable

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -47,7 +47,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $lastname;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $ProfilePicture;
 
