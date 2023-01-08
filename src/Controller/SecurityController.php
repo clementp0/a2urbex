133232a7afa9a32c1f715d60070eda9ebe51e542
Clement Po <clement.pommerie@gmail.com>
2023-01-08 05:17:33 +0100

    Security + Form Design

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/connexion", name="app_login")
+     * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -27,7 +27,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/deconnexion", name="app_logout")
+     * @Route("/logout", name="app_logout")
      */
     public function logout(): void
     {
