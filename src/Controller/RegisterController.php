dab0167ec897d39d66c4212e07d7a5d5a52dda29
Lahaxe Hugo <lahaxeh@gmail.com>
2023-01-12 22:57:34 +0100

    adding default favorite playlist on register

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -6,10 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use App\Entity\Favorite;
 use App\Form\RegisterType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Repository\FavoriteRepository;
 
 class RegisterController extends AbstractController
 {
@@ -25,7 +27,7 @@ class RegisterController extends AbstractController
     /**
      * @Route("/register", name="app_register")
      */
-    public function index(Request $request, UserPasswordEncoderInterface $encoder)
+    public function index(Request $request, UserPasswordEncoderInterface $encoder, FavoriteRepository $favoriteRepository)
     { 
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -41,7 +43,10 @@ class RegisterController extends AbstractController
 
             $this->entityManger->persist($user);
             $this->entityManger->flush();
-          
+
+            $fav = new Favorite();
+            $fav->setName('like')->setMaster(1)->addUser($user);
+            $favoriteRepository->save($fav, true);
         }
         return $this->render('register/index.html.twig', [
             'form' => $form->createView(),
