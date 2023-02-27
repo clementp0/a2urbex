2b0d00d126ccc12e28fd53793048db32497cf342
Clement Po <clement.pommerie@gmail.com>
2023-02-27 23:59:29 +0100

    Pwa - No notifications

---
+++ public/assets/js/notifications.js
@@ -0,0 +1,21 @@
+function displayNotification(title, message) {
+  if (Notification.permission === "granted") {
+    // If the user has granted permission to display notifications
+    var notification = new Notification(title, {
+      body: message,
+      icon: '/a2urbex192x192.png'
+    });
+  } else if (Notification.permission !== "denied") {
+    // If the user has not yet granted or denied permission to display notifications
+    Notification.requestPermission().then(function (permission) {
+      // If permission is granted, display the notification
+      if (permission === "granted") {
+        var notification = new Notification(title, {
+          body: message,
+          icon: '/a2urbex192x192.png'
+        });
+      }
+    });
+  }
+}
+
