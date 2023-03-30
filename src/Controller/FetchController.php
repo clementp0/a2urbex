61d17c37fb48029c6b86a1e4c278c7f882cc4442
Clement Po <clement.pommerie@gmail.com>
2023-03-30 17:50:42 +0200

    Testing

--- src/Controller/FetchController.php
+++ src/Controller/FetchController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use App\Entity\Location;
 use App\Repository\LocationRepository;
 use App\Service\LocationService;
