40c1ca5891c93df962871670dd7a0df931b95f24
Clement Po <clement.pommerie@gmail.com>
2023-03-31 19:20:38 +0200

    After Login Redirect

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -49,7 +49,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-      return new RedirectResponse($this->urlGenerator->generate('admin'));
+      return new RedirectResponse($this->urlGenerator->generate('app_location_index'));
     }
 
     protected function getLoginUrl(Request $request): string
