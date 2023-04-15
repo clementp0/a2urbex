d1d78c4da24468f1a8d2b1deef0d9b458bed985b
Clement Po <clement.pommerie@gmail.com>
2023-04-15 18:10:10 +0200

    Removing Pending - Option

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -104,7 +104,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = '';
+        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
