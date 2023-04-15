f40ecde81d43d9ab8cc70ae168e21408cf2e0766
Lahaxe Hugo <lahaxeh@gmail.com>
2023-04-15 15:22:26 +0200

    replacing online users by online friends

--- src/Service/UserOnlineService.php
+++ src/Service/UserOnlineService.php
@@ -4,15 +4,18 @@
 namespace App\Service;
 
 use App\Entity\User;
+use App\Entity\Friend;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
 
 class UserOnlineService
 {
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, Security $security)
     {
         $this->entityManager = $entityManager;
+        $this->security = $security;
     }
 
     public function addUser(User $user): void
@@ -32,47 +35,56 @@ class UserOnlineService
 
     public function getOnlineUsers(): array
     {
-    $threshold = new \DateTime('-5 minutes');
+        $threshold = new \DateTime('-5 minutes');
+        $cuser = $this->security->getUser();
 
-    $onlineUsers = $this->entityManager->getRepository(User::class)->createQueryBuilder('u')
-        ->select('u.firstname', 'u.lastname')
-        ->where('u.lastActiveAt >= :threshold')
-        ->setParameter('threshold', $threshold)
-        ->getQuery()
-        ->getResult();
-
-    
-
-    $offlineUsers = $this->entityManager->getRepository(User::class)->createQueryBuilder('u')
-        ->where('u.lastActiveAt < :threshold')
-        ->setParameter('threshold', $threshold)
-        ->getQuery()
-        ->getResult();
-
-    foreach ($offlineUsers as $user) {
-        $lastActiveAt = $user->getLastActiveAt();
-        $diff = $lastActiveAt->diff(new \DateTime())->format('%a:%h:%i');
-        list($days, $hours, $minutes) = explode(':', $diff);
-        $totalMinutes = ($days * 24 * 60) + ($hours * 60) + $minutes;
-        if ($totalMinutes < 59) {
-            $user = [
-                'firstname' => $user->getFirstname(),
-                'lastname' => substr($user->getLastname(), 0, 1),
-                'active' => ' (' . $minutes . 'm ago)',
-                'status' => 'away'
-            ];
+        if(!$cuser->hasRole('ROLE_ADMIN')) {
+            $users = [$cuser->getId()];
+            $f = $this->entityManager->getRepository(Friend::class)->findFriendForSearch($cuser->getId());
+            if($f) foreach($f as $item) $users[] = $item['id'];
         }
-        else {
+        
+        $onlineUsers = $this->entityManager->getRepository(User::class)->createQueryBuilder('u')
+            ->select('u.firstname', 'u.lastname')
+            ->where('u.lastActiveAt >= :threshold')
+            ->setParameter('threshold', $threshold);
+
+        if(!$cuser->hasRole('ROLE_ADMIN')) $onlineUsers->andWhere('u.id IN ('.implode(', ', $users).')');
+        $onlineUsers = $onlineUsers->getQuery()->getResult();
+
+        
+
+        $offlineUsers = $this->entityManager->getRepository(User::class)->createQueryBuilder('u')
+            ->where('u.lastActiveAt < :threshold')
+            ->setParameter('threshold', $threshold);
+
+        if(!$cuser->hasRole('ROLE_ADMIN')) $offlineUsers->andWhere('u.id IN ('.implode(', ', $users).')');
+        $offlineUsers = $offlineUsers->getQuery()->getResult();
+
+        foreach ($offlineUsers as $user) {
+            $lastActiveAt = $user->getLastActiveAt();
+            $diff = $lastActiveAt->diff(new \DateTime())->format('%a:%h:%i');
+            list($days, $hours, $minutes) = explode(':', $diff);
+            $totalMinutes = ($days * 24 * 60) + ($hours * 60) + $minutes;
+            if ($totalMinutes < 59) {
                 $user = [
-                'firstname' => $user->getFirstname(),
-                'lastname' => substr($user->getLastname(), 0, 1),
-                'active' => '(Offline)',
-                'status' => 'offline'
-            ];
+                    'firstname' => $user->getFirstname(),
+                    'lastname' => substr($user->getLastname(), 0, 1),
+                    'active' => ' (' . $minutes . 'm ago)',
+                    'status' => 'away'
+                ];
+            }
+            else {
+                $user = [
+                    'firstname' => $user->getFirstname(),
+                    'lastname' => substr($user->getLastname(), 0, 1),
+                    'active' => '(Offline)',
+                    'status' => 'offline'
+                ];
+            }
+            $onlineUsers[] = $user;
         }
-        $onlineUsers[] = $user;
-    }
 
-    return $onlineUsers;
-}
+        return $onlineUsers;
+    }
 }
