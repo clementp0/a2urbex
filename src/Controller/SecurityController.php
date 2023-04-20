ec99a1423014e8ea7185be481dfc8b4da4bcb402
Lahaxe Hugo <lahaxeh@gmail.com>
2023-04-20 18:10:15 +0200

    moving /register to security controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,9 +9,21 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use App\Repository\UserRepository;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\FavoriteRepository;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use App\Entity\User;
+use App\Entity\Favorite;
+use App\Form\RegisterType;
 
 class SecurityController extends AppController
 {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManger = $entityManager;         
+    }
+
     #[Route('/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -29,6 +41,36 @@ class SecurityController extends AppController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function index(Request $request, UserPasswordEncoderInterface $encoder, FavoriteRepository $favoriteRepository)
+    { 
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+           
+            $user = $form->getData();
+
+            $password = $encoder->encodePassword($user,$user->getPassword());
+
+            $user->setPassword($password);
+
+            $this->entityManger->persist($user);
+            $this->entityManger->flush();
+
+            $fav = new Favorite();
+            $fav->setName('like')->setMaster(1)->addUser($user);
+            $favoriteRepository->save($fav, true);
+
+            return new RedirectResponse($this->generateUrl('app_login'));
+
+        }
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
 
