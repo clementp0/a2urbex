305c982ce1a93ac63b9d9a6355ded0bb46c9d4ef
Lahaxe Hugo <lahaxeh@gmail.com>
2023-05-02 21:35:42 +0200

    refacto secure chat & use message entity

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -5,39 +5,80 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+
+use App\Repository\MessageRepository;
+use App\Entity\Message;
 
 class ChatController extends AbstractController
 {
 
+    private $ignoreList = [
+        'favorites', 
+        'locations', 
+        'friends', 
+        'friendRequests', 
+        'password',
+        'lastname',
+        'salt',
+        'username',
+        'userIdentifier',
+        'email',
+        'lastActiveAt'
+    ];
 
-     #[Route('/chat-add', name: 'save_chat_history', methods: ['GET', 'POST'])]
-    public function saveChatHistory(Request $request): Response
+    #[Route('/chat-add', name: 'save_chat_history', methods: ['GET', 'POST'])]
+    public function saveChatHistory(Request $request, MessageRepository $messageRepository): Response
     {
-        $chatHistory = $request->getContent();
-        $filename = 'chat.json';
-        $filePath = $this->getParameter('kernel.project_dir') . '/public/' . $filename;
-        $file = fopen($filePath, 'a');
-        fwrite($file, ',' . PHP_EOL . $chatHistory );
-        fclose($file);
+        $user = $this->getUser();
+        if($user) {
+            $messageContent = $request->getContent();
 
-        return new Response('Chat history saved.');
+            $message = new Message();
+            $message
+                ->setSender($user)
+                ->setMessage($messageContent)
+                ->setGlobal(1)
+                ->setDateTime(new \DateTime('@'.strtotime('now')))
+            ;
 
+            $messageRepository->save($message, true);
+
+            $serializer = $this->container->get('serializer');
+            
+            $return = $serializer->serialize(
+                $message, 
+                'json', 
+                [
+                    'circular_reference_handler' => function ($object) {return $object->getId(); },
+                    AbstractNormalizer::IGNORED_ATTRIBUTES => $this->ignoreList
+                ]
+            );
+
+            return new Response($return);
+        } else {
+            return new JsonResponse(['error' => 'reload']);
+        }
     }
 
 
     #[Route('/chat-get', name: 'chat_history', methods: ['GET', 'POST'])]
-    
-    public function getChatHistory(): Response
+    public function getChatHistory(MessageRepository $messageRepository): Response
     {
-        $filename = 'chat.json';
-        $filePath = $this->getParameter('kernel.project_dir') . '/public/' . $filename;
-        $chatHistory = '';
-        if (file_exists($filePath)) {
-            $file = fopen($filePath, 'r');
-            $chatHistory = fread($file, filesize($filePath));
-            fclose($file);
-        }
-        return new Response('[' . $chatHistory . ']');
+        $messages = $messageRepository->findBy(['global' => 1]);
+
+        $serializer = $this->container->get('serializer');
+        $return = $serializer->serialize(
+            $messages, 
+            'json', 
+            [
+                'circular_reference_handler' => function ($object) {return $object->getId(); },
+                AbstractNormalizer::IGNORED_ATTRIBUTES => $this->ignoreList
+            ]
+        );
+
+        return new Response($return);
     }
 }
