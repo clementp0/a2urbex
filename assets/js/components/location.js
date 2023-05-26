858096cc1e2993ee362956bbed39134248f31896
Lahaxe Hugo <lahaxeh@gmail.com>
2023-05-26 14:16:42 +0200

    rework location js

--- assets/js/components/location.js
+++ assets/js/components/location.js
@@ -1,22 +1,20 @@
 $(() => {
   $('.coordinates').insertAfter('#new_location_lon')
   $('.coordinates').insertAfter('#location_lon')
-  $(document).ready(function () {
-    $('#new_location_lon, #new_location_lat').keyup(function () {
-      var lon = $('#new_location_lon').val()
-      var lat = $('#new_location_lat').val()
-      var link = 'https://www.google.com/maps?t=k&q=' + lat + ',' + lon
-      $('.coordinates').attr('href', link)
-    })
+
+  $('#new_location_lon, #new_location_lat').on('keyup', function () {
+    const lon = $('#new_location_lon').val()
+    const lat = $('#new_location_lat').val()
+    const link = 'https://www.google.com/maps?t=k&q=' + lat + ',' + lon
+    $('.coordinates').attr('href', link)
   })
-  $(document).ready(function () {
-    $('#location_lon, #location_lat').keyup(function () {
-      var lon = $('#location_lon').val()
-      var lat = $('#location_lat').val()
-      var link = 'https://www.google.com/maps?t=k&q=' + lat + ',' + lon
-      $('.coordinates').attr('href', link)
-    })
+  $('#location_lon, #location_lat').on('keyup', function () {
+    const lon = $('#location_lon').val()
+    const lat = $('#location_lat').val()
+    const link = 'https://www.google.com/maps?t=k&q=' + lat + ',' + lon
+    $('.coordinates').attr('href', link)
   })
+
   function previewImage(event) {
     const input = event.target
     if (input.files && input.files[0]) {
@@ -29,43 +27,41 @@ $(() => {
       reader.readAsDataURL(input.files[0])
     }
   }
-  var fileInput = document.getElementById('new_location_image')
-  var fileSize = document.getElementById('fileSize')
+  window.previewImage = previewImage
 
-  fileInput.addEventListener('change', function () {
-    if (fileInput.files.length > 0) {
-      var sizeInBytes = fileInput.files[0].size
-      var sizeInMB = (sizeInBytes / 1000000).toFixed(2)
-      fileSize.innerHTML = ' (Selected : ' + sizeInMB + 'MB,<span class="max-size"> Max 8MB</span>)'
+  $('#new_location_image').on('change', function () {
+    if (this.files.length > 0) {
+      const sizeInBytes = this.files[0].size
+      const sizeInMB = (sizeInBytes / 1000000).toFixed(2)
+      $('#fileSize').html(' (Selected : ' + sizeInMB + 'MB,<span class="max-size"> Max 8MB</span>)')
     }
   })
 
   window.onload = function () {
-    var imagepreview = document.getElementById('image-preview')
-    var uploader = document.querySelector('.custom-file-label')
-    uploader.appendChild(imagepreview)
-    var size = document.getElementById('fileSize')
-    var uploaderlabel = document.querySelector('.image-label-placeholder')
-    uploaderlabel.appendChild(size)
-    var error = document.getElementById('error')
-    var formtype = document.querySelector('#new_location')
-    formtype.appendChild(error)
+    $('.custom-file-label').append($('#image-preview'))
+    $('.image-label-placeholder').append($('#fileSize'))
+    $('#new_location').append($('#error'))
   }
 
-  const form = document.querySelector('form[name="new_location"]')
-  form.addEventListener('submit', function (event) {
-    const fileInput = document.querySelector('#new_location_image')
-    const fileName = fileInput.value
+  const form = $('form[name="new_location"]')
+  form.on('submit', function (event) {
+    const fileName = $('#new_location_image').val()
     const fileExtension = fileName.split('.').pop().toLowerCase()
 
+    console.log(fileName, fileExtension)
+    event.preventDefault()
+
     if (fileName.length && !['jpg', 'jpeg', 'png'].includes(fileExtension)) {
-      event.preventDefault()
       const errorMessage =
-        '<span class="alert alert-danger d-block"><span class="d-block"><span class="form-error-icon badge badge-danger text-uppercase">Error</span> <span class="form-error-message">Invalid File Type.</span></span></span>'
-      const errorElement = document.querySelector('#error')
-      errorElement.innerHTML = errorMessage
-      const newLocationElement = document.querySelector('#new_location')
-      newLocationElement.insertBefore(errorElement, newLocationElement.firstChild)
+        '<span class="alert alert-danger d-block">' +
+        ' <span class="d-block">' +
+        '   <span class="form-error-icon badge badge-danger text-uppercase">Error</span>' +
+        '   <span class="form-error-message">Invalid File Type.</span>' +
+        ' </span>' +
+        '</span>'
+      $('#error').html(errorMessage)
+      $('#new_location').prepend($('#error'))
+      event.preventDefault()
     }
   })
 })
