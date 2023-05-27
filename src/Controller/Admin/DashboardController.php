366c5e7ab711b3fccd3751048b9b78e812b0b1dd
Lahaxe Hugo <lahaxeh@gmail.com>
2023-05-27 21:52:22 +0200

    using config instead of data file in admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -33,76 +33,22 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         $em = $this->getDoctrine()->getManager();
-
-        //Location_count
         $repoLocation = $em->getRepository(Location::class);
-        $pins_count = $repoLocation->createQueryBuilder('a')
-            ->select('count(a.id)')
-            ->getQuery()
-            ->getSingleScalarResult();
-
-        //pending 
-        $pending = $repoLocation->createQueryBuilder('a')
-            ->select('count(a.id)')
-            ->where('a.pending = 1')
-            ->getQuery()
-            ->getSingleScalarResult();
-
-        //Country_count
         $repoCountry = $em->getRepository(Country::class);
-        $country_count = $repoCountry->createQueryBuilder('a')
-            ->select('count(a.id)')
-            ->getQuery()
-            ->getSingleScalarResult();
-
-        //Type_count
         $repoType = $em->getRepository(Type::class);
-        $type_count = $repoType->createQueryBuilder('a')
-            ->select('count(a.id)')
-            ->getQuery()
-            ->getSingleScalarResult();
-
-        //Curent time
-        $current_time = date("d/m/Y H:i", time());
-
-        //Last fetched
-        $array = $this->dataService->getJson($this->getParameter('data_directory').'export.json');
-        $last_fetched = $array["last_fetched"];
-
-        //Last updated
-        $array_updated = $this->dataService->getJson($this->getParameter('data_directory').'update.json');
-        $last_updated = $array_updated["last_updated"];
-
-        //Output
-        $board = $array["board"];
-        $finished = $array["finished"];
-        $error = $array["error"];
-        $total = $array["total"];
-        $newpins = $array["newpins"];
-        $token = $array["token"];
-        $updated = $array_updated["last_updated"];
-
-        //Upload list
         $repoUpload = $em->getRepository(Upload::class);
-        $uploads = $repoUpload->findAll();
-        $uploads_count = $repoUpload->createQueryBuilder('a')
-            ->select('count(a.id)')
-            ->getQuery()
-            ->getSingleScalarResult();
+
+        $location_count = $repoLocation->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+        $pending_count = $repoLocation->createQueryBuilder('a')->select('count(a.id)')->where('a.pending = 1')->getQuery()->getSingleScalarResult();
+        $country_count = $repoCountry->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+        $type_count = $repoType->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+        $upload_count = $repoUpload->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+
+        $to_be_generated = $repoLocation->createQueryBuilder('a')->select('count(a.id)')->where('a.image IS NULL')->getQuery()->getSingleScalarResult();
+        $ai = $repoLocation->createQueryBuilder('a')->where('a.ai = true')->select('count(a.id)')->getQuery()->getSingleScalarResult();
 
         //AI Generation
-        $to_be_generated = $repoLocation->createQueryBuilder('a')
-            ->select('count(a.id)')
-            ->where('a.image IS NULL')
-            ->getQuery()
-            ->getSingleScalarResult();
-        $ai = $repoLocation->createQueryBuilder('a')
-            ->where('a.ai = true')
-            ->select('count(a.id)')
-            ->getQuery()
-            ->getSingleScalarResult();
         $port = $_ENV['STABLE_PORT'];
-
         $url = 'http://127.0.0.1:7860/';
         $headers = @get_headers($url);
         if ($headers && strpos($headers[0], '200') !== false) {
@@ -113,31 +59,26 @@ class DashboardController extends AbstractDashboardController
             $stable_status_current = "off";
         }
 
-
         //Return data 
         return $this->render('admin/index.html.twig', [
-            'websocket' => $_ENV["WEBSOCKET_URL"],
-            'pins' => $pins_count,
-            'country' => $country_count,
-            'type' => $type_count,
-            'current_time' => $current_time,
-            'last_fetched' => $last_fetched,
-            'board' => $board,
-            'finished' => $finished,
-            'error' => $error,
-            'total' => $total,
-            'newpins' => $newpins,
-            'token' => $token,
-            'last_updated' => $updated,
-            'uploads' => $uploads,
-            'uploads_count' => $uploads_count,
+            'location_count' => $location_count,
+            'pending_count' => $pending_count,
+            'country_count' => $country_count,
+            'type_count' => $type_count,
+            'upload_count' => $upload_count,
+
             'to_be_generated' => $to_be_generated,
+            'ai' => $ai,
             'port' => $port,
             'stable_status' => $stable_status,
             'stable_status_current' => $stable_status_current,
-            'ai' => $ai,
-            'remaining' => $this->dataService->getFile($this->getParameter('data_directory').'count.txt'),
-            'pending' => $pending
+
+            'current_time' => date("d/m/Y H:i", time()),
+            'websocket' => $_ENV["WEBSOCKET_URL"],
+            'uploads' => $repoUpload->findAll(),
+
+            'pinterest' => $this->configRepository->get('pinterest'),
+            'wikimapia' => $this->configRepository->get('wikimapia'),
         ]);
     }
 
