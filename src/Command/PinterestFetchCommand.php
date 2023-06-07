f2da4b4f301414a581053030bf791d61f1a4eb63
Lahaxe Hugo <lahaxeh@gmail.com>
2023-06-07 20:31:29 +0200

    adding pinterest command

---
+++ src/Command/PinterestFetchCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class PinterestFetchCommand extends Command
+{
+    protected static $defaultName = "pinterest:fetch";
+
+    public function __construct(private ContainerInterface $container)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $controller = $this->container->get('App\Controller\FetchController');
+        $response = $controller->fetchPinterest();
+        
+        return Command::SUCCESS;
+    }
+}
