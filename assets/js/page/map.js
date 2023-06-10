ebc7cf649c6e1b628f7b7d3b45a5da0f6241648e
Lahaxe Hugo <lahaxeh@gmail.com>
2023-06-10 17:16:15 +0200

    open popup on search

--- assets/js/page/map.js
+++ assets/js/page/map.js
@@ -45,84 +45,77 @@ function setMarkers(map, items) {
   pins['default'].url = pins['default'].url + 'default.png'
   pins['default'].zIndex = 1
 
-  for (let key in items) {
+  items.forEach((item) => {
     let current = pins['default']
-    if (items[key].loc.type) {
-      if (!pins[items[key].loc.type.icon]) {
-        pins[items[key].loc.type.icon] = { ...icon }
-        pins[items[key].loc.type.icon].url =
-          pins[items[key].loc.type.icon].url + items[key].loc.type.icon + '.png'
+    if (item.loc.type) {
+      if (!pins[item.loc.type.icon]) {
+        pins[item.loc.type.icon] = { ...icon }
+        pins[item.loc.type.icon].url = pins[item.loc.type.icon].url + item.loc.type.icon + '.png'
       }
 
-      current = pins[items[key].loc.type.icon]
+      current = pins[item.loc.type.icon]
     }
 
     const marker = new google.maps.Marker({
       position: {
-        lat: Number.parseFloat(items[key].loc.lat),
-        lng: Number.parseFloat(items[key].loc.lon),
+        lat: Number.parseFloat(item.loc.lat),
+        lng: Number.parseFloat(item.loc.lon),
       },
       map,
       icon: current,
       shape: shape,
-      title: items[key].loc.name,
-      type: items[key].loc.type,
+      title: item.loc.name,
+      type: item.loc.type,
       zIndex: current.zIndex,
     })
 
     marker.addListener('click', () => {
-      if (items[key].loc.disabled == 1) {
-        $('.map-overlay-img').attr('id', 'disabled')
-      } else {
-        $('.map-overlay-img').removeAttr('id')
-      }
-
-      $('.pin-fav-add').removeClass('show')
-      $('#map-overlay').addClass('show')
-      if (items[key].loc.image === null) {
-        $('#map-overlay .map-overlay-img').css('backgroundImage', 'url("/assets/default.png")')
-      } else {
-        $('#map-overlay .map-overlay-img').css(
-          'backgroundImage',
-          'url(' + items[key].loc.image + ')'
-        )
-      }
-      if (items[key].loc.name) $('#map-overlay .map-overlay-title').text(items[key].loc.name)
-      $('#map-overlay .map-overlay-type .pin-type-text').text(
-        items[key].loc.type !== null ? items[key].loc.type.name : 'other'
-      )
-      $('#map-overlay .map-overlay-type .pin-type-icon').html(
-        items[key].loc.type !== null
-          ? '<i class="fa-solid ' + items[key].loc.type.icon + '"></i>'
-          : '<i class="fa-solid fa-map-pin"></i>'
-      )
-
-      $('.pin-fav-wrapper').attr('data-id', items[key].loc.lid)
-      $('.pin-fav-wrapper').attr('data-fids', items[key].fids)
-
-      let mapsUrl =
-        $('.map-overlay-action .pin-map').data('url') +
-        items[key].loc.lat +
-        ',' +
-        items[key].loc.lon
-      $('.map-overlay-action .pin-map').attr('href', mapsUrl)
-      let editUrl = $('.map-overlay-action .pin-conf')
-        .data('url')
-        .replace('-key-', items[key].loc.lid)
-      $('.map-overlay-action .pin-conf').attr('href', editUrl)
-      let wazeUrl =
-        $('.map-overlay-action .pin-waze').data('url') +
-        items[key].loc.lat +
-        ',' +
-        items[key].loc.lon +
-        '&navigate=yes&zoom=17'
-      $('.map-overlay-action .pin-waze').attr('href', wazeUrl)
-
-      if (items[key].fids)
-        $('#map-overlay').find('.pin-fav i').addClass('fa-solid').removeClass('fa-regular')
-      else $('#map-overlay').find('.pin-fav i').addClass('fa-regular').removeClass('fa-solid')
+      popup(item)
     })
+  })
+}
+
+function popup(item) {
+  if (item.loc.disabled == 1) {
+    $('.map-overlay-img').attr('id', 'disabled')
+  } else {
+    $('.map-overlay-img').removeAttr('id')
   }
+
+  $('.pin-fav-add').removeClass('show')
+  $('#map-overlay').addClass('show')
+  if (item.loc.image === null) {
+    $('#map-overlay .map-overlay-img').css('backgroundImage', 'url("/assets/default.png")')
+  } else {
+    $('#map-overlay .map-overlay-img').css('backgroundImage', 'url(' + item.loc.image + ')')
+  }
+  if (item.loc.name) $('#map-overlay .map-overlay-title').text(item.loc.name)
+  $('#map-overlay .map-overlay-type .pin-type-text').text(
+    item.loc.type !== null ? item.loc.type.name : 'other'
+  )
+  $('#map-overlay .map-overlay-type .pin-type-icon').html(
+    item.loc.type !== null
+      ? '<i class="fa-solid ' + item.loc.type.icon + '"></i>'
+      : '<i class="fa-solid fa-map-pin"></i>'
+  )
+
+  $('.pin-fav-wrapper').attr('data-id', item.loc.lid)
+  $('.pin-fav-wrapper').attr('data-fids', item.fids)
+
+  let mapsUrl = $('.map-overlay-action .pin-map').data('url') + item.loc.lat + ',' + item.loc.lon
+  $('.map-overlay-action .pin-map').attr('href', mapsUrl)
+  let editUrl = $('.map-overlay-action .pin-conf').data('url').replace('-key-', item.loc.lid)
+  $('.map-overlay-action .pin-conf').attr('href', editUrl)
+  let wazeUrl =
+    $('.map-overlay-action .pin-waze').data('url') +
+    item.loc.lat +
+    ',' +
+    item.loc.lon +
+    '&navigate=yes&zoom=17'
+  $('.map-overlay-action .pin-waze').attr('href', wazeUrl)
+
+  if (item.fids) $('#map-overlay').find('.pin-fav i').addClass('fa-solid').removeClass('fa-regular')
+  else $('#map-overlay').find('.pin-fav i').addClass('fa-regular').removeClass('fa-solid')
 }
 
 function setUserPosition(map) {
@@ -185,43 +178,12 @@ function search(map, json, inputWrapper, value) {
         new google.maps.LatLng(Number.parseFloat(item.loc.lat), Number.parseFloat(item.loc.lon))
       )
       map.setZoom(10)
+
+      popup(item)
     })
   })
 
   inputResult.removeClass('hidden')
-}
-
-function search2(map, searchBox, markers) {
-  // const places = searchBox.getPlaces()
-  // if (places.length == 0) return
-  // console.log(markers)
-  // markers.m.forEach((marker) => marker.setMap(null))
-  // markers.m = []
-  // const bounds = new google.maps.LatLngBounds()
-  // places.forEach((place) => {
-  //   if (!place.geometry || !place.geometry.location) {
-  //     console.log('Returned place contains no geometry')
-  //     return
-  //   }
-  //   const icon = {
-  //     url: place.icon,
-  //     size: new google.maps.Size(71, 71),
-  //     origin: new google.maps.Point(0, 0),
-  //     anchor: new google.maps.Point(17, 34),
-  //     scaledSize: new google.maps.Size(25, 25),
-  //   }
-  //   markers.m.push(
-  //     new google.maps.Marker({
-  //       map,
-  //       icon,
-  //       title: place.name,
-  //       position: place.geometry.location,
-  //     })
-  //   )
-  //   if (place.geometry.viewport) bounds.union(place.geometry.viewport)
-  //   else bounds.extend(place.geometry.location)
-  // })
-  // map.fitBounds(bounds)
 }
 
 window.initMap = initMap
