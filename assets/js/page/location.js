262e521b24d46374a220b75d1f0a6d170585b062
Lahaxe Hugo <lahaxeh@gmail.com>
2023-06-11 03:03:03 +0200

    fix location js

--- assets/js/page/location.js
+++ assets/js/page/location.js
@@ -1,13 +1,15 @@
 import Coord from '../components/coord'
 
 $(() => {
-  Coord.init('#location', '#location_lat', '#location_lon')
+  if ($('#location').length) {
+    Coord.init('#location', '#location_lat', '#location_lon')
 
-  $('.coordinates').insertAfter('#location_lon')
-  $('#location_lat, #location_lon').on('keyup', function () {
-    const lat = $('#location_lat').val()
-    const lon = $('#location_lon').val()
-    const link = 'https://www.google.com/maps?t=k&q=' + lat + ',' + lon
-    $('.coordinates').attr('href', link)
-  })
+    $('.coordinates').insertAfter('#location_lon')
+    $('#location_lat, #location_lon').on('keyup', function () {
+      const lat = $('#location_lat').val()
+      const lon = $('#location_lon').val()
+      const link = 'https://www.google.com/maps?t=k&q=' + lat + ',' + lon
+      $('.coordinates').attr('href', link)
+    })
+  }
 })
