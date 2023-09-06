f20f63e642edc77961819c9c34f1b0273a436924
clementp0 <clement.pommerie@gmail.com>
2023-09-06 21:13:10 +0200

    Feat : stats real data

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -2,17 +2,87 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Entity\Location;
+use App\Entity\Country;
+use App\Entity\Category;
+use App\Entity\Source;
+use App\Repository\LocationRepository;
+use App\Repository\MessageRepository;
+use App\Service\DataService;
+use App\Repository\ConfigRepository;
+use Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\WebsocketService;
 
 class StatusController extends AbstractController
 {
-    #[Route('/status', name: 'app_status', methods: ['GET'])]
+    public function __construct(
+        private DataService $dataService,
+        private ConfigRepository $configRepository,
+        private EntryFilesTwigExtension $entryFilesTwigExtension,
+        private WebsocketService $websocketService
+    ) {}
+
+    #[Route('/status', name: 'app_status')]
     public function index(): Response
     {
-        return $this->render('status/index.html.twig', [
-            'controller_name' => 'StatusController',
+        $em = $this->getDoctrine()->getManager();
+        $repoLocation = $em->getRepository(Location::class);
+        $repoCountry = $em->getRepository(Country::class);
+        $repoCategory = $em->getRepository(Category::class);
+        $repoSource = $em->getRepository(Source::class);
+        $repoUser = $em->getRepository(User::class);
+
+        $user_count = $repoUser->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+        $location_count = $repoLocation->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+        $pending_count = $repoLocation->createQueryBuilder('a')->select('count(a.id)')->where('a.pending = 1')->getQuery()->getSingleScalarResult();
+        $country_count = $repoCountry->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+        $category_count = $repoCategory->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+        $source_count = $repoSource->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+        $ai_wainting_count = $repoLocation->createQueryBuilder('a')->select('count(a.id)')->where('a.image IS NULL')->getQuery()->getSingleScalarResult();
+        $ai_count = $repoLocation->createQueryBuilder('a')->where('a.ai = true')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+
+        //AI Generation
+        $ai_port = $_ENV['STABLE_PORT'];
+        $url = 'http://127.0.0.1:7860/';
+        $headers = @get_headers($url);
+        $ai_status = $headers && strpos($headers[0], '200') !== false;
+
+        //Return data 
+        return $this->render('/status/index.html.twig', [
+            'location_count' => $location_count,
+            'pending_count' => $pending_count,
+            'country_count' => $country_count,
+            'category_count' => $category_count,
+            'source_count' => $source_count,
+            'ai_wainting_count' => $ai_wainting_count,
+            'ai_count' => $ai_count,
+            'user_count' => $user_count,
+
+            'ai_port' => $ai_port,
+            'ai_status' => $ai_status,
+
+            'current_time' => date("d/m/Y H:i", time()),
+            'sources' => $repoSource->findAll(),
+            'websocket' => $_ENV["WEBSOCKET_URL"],                  
+
+            'pinterest' => $this->configRepository->get('pinterest'),
+            'wikimapia' => $this->configRepository->get('wikimapia'),
         ]);
     }
+
+
+
+    //Basic config
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('<i class="fas fa-globe-europe"></i> a2urbex ')
+            ->setFaviconPath('favicon.ico');
+    }
+
 }
